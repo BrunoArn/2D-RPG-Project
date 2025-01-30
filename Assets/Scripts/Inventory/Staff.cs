5cb4b000afb5c8666c302b775a35594d67a52047
Bruno Arnaud <bruno.arnaud.br@gmail.com>
2025-01-30 15:55:15 -0300

    add staff and bow visual

--- Assets/Scripts/Inventory/Staff.cs
+++ Assets/Scripts/Inventory/Staff.cs
@@ -3,8 +3,33 @@ using UnityEngine;
 public class Staff : MonoBehaviour, IWeapon
 {
 
+    private void Update() {
+        MouseFollowWithOffSet();
+    }
+
     public void Attack() {
         Debug.Log("Staff Atacck");
         ActiveWeapon.Instance.ToggleIsAttacking(false);
+    }
+
+    private void MouseFollowWithOffSet() {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(PlayerController.Instance.transform.position);
+
+        float deltaX = mousePos.x - playerScreenPoint.x;
+        float deltaY = mousePos.y - playerScreenPoint.y;
+
+        // Varia bastante comum offset no player tbm
+        //float angle = Mathf.Atan2(deltaY, Mathf.Abs(deltaX)) * Mathf.Rad2Deg;
+
+        //varia menos e fica mais lateralizado
+        float angle = Mathf.Atan2(mousePos.y, Mathf.Abs(mousePos.x)) * Mathf.Rad2Deg;
+
+        // espada vira pro lado
+        if (mousePos.x < playerScreenPoint.x) {
+            ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, -180, angle);
+        } else {
+            ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
     }
 }
